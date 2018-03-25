2bd7c97684f1455bc367f616e2e97ab2747d6b4b
elir de assis ribeiro <elirweb@hotmail.com>
2018-03-25 20:23:28 -0300

    colocando design parthen

---
+++ Design/Style/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Style
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estilo de Design Parthen");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Design/Style/Strategy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Style
+{
+    public interface Strategy
+    {
+        Guid NumeroPedido(); 
+    }
+
+    public class PedidoCompra : Strategy
+    {
+        // a interface stra
+        public Guid NumeroPedido()
+        {
+            return Guid.NewGuid();
+        }
+    }
+
+    public class PedidoSolicitacao : Strategy
+    {
+        public Guid NumeroPedido()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
